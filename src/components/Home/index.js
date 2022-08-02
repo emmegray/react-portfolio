0f9e8b73e83119d66e48772d116bc908b813c387
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2022-08-02 23:46:01 +0200

    Added Logo comp svg animation on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 import './index.scss';
 
 const Home = () => {
@@ -35,6 +36,7 @@ const Home = () => {
         <h2>Frontend Junior Dev / Illustrator / Streamer</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
+      <Logo></Logo>
     </div>
     </>
   )
